d2f75701a894b75eb72aa84f72acac60c481c30d
Lau <arinel88@gmail.com>
2020-09-21 09:38:36 +0200

    Finito esercizio

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+
+
+// Faccio la chiamata all'AJAX
+
+
   $.ajax(
     {
       "url" :"https://flynn.boolean.careers/exercises/api/array/music",
@@ -7,14 +12,31 @@ $(document).ready(function() {
       "success": function (data, status) {
         var results = data.response;
         render(results);
+
       },
       "error": function (richiesta, stato, errori) {
         alert("E' avvenuto un errore. " + errori);
       }
     }
   );
+
+// Faccio in modo che al cambio della select mostri solo i generi selezionati
+
+  $("#selected").change(function(){
+    var genreSelected = $(this).val();
+
+    if(genreSelected == "" ) {
+      $(".cd").show();
+    } else {
+      $(".cd").hide();
+      $(".cd[data-genere ='"+genreSelected+"']").show();
+    }
+  });
+
+
 });
 
+// Inizializzo la funzione di render
 function render(results) {
 
   var source = $("#album-template").html();
@@ -22,9 +44,11 @@ function render(results) {
 
   for (var i=0; i< results.length; i++) {
     var album = results[i];
-    var html = template(album);
 
+    var genereAlbum = album.genre.toLowerCase();
+
+    var html = template(album);
     $(".cds-container").append(html);
-  }
+  };
 
 }
